01e882a47a426044b89439f0f5b0f1ac9d956a99
HaixiangYan <haixiang6123@gmail.com>
2021-04-10 11:35:02 +0800

    feat: 添加子组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, {createContext, useContext, useState} from 'react';
 
-function App() {
+const AppContext = createContext({})
+
+const User = () => {
+  return <div>User</div>
+}
+
+const UserModify = () => {
+  return <div>UserModify</div>
+}
+
+const FirstSon = () => {
   return (
-    <div>App</div>
+    <section>
+      大儿子
+      <User/>
+    </section>
+  )
+}
+
+const SecondSon = () => {
+  return (
+    <section>
+      二儿子
+      <UserModify/>
+    </section>
+  )
+}
+
+const ThirdSon = () => {
+  return <section>三儿子</section>
+}
+
+const App = () => {
+  const [appState, setAppState] = useState({
+    user: {user: 'name', age: 18}
+  })
+
+  const contextValue = {appState, setAppState}
+
+  return (
+    <AppContext.Provider value={contextValue}>
+      <FirstSon/>
+      <SecondSon/>
+      <ThirdSon/>
+    </AppContext.Provider>
   );
 }
 
